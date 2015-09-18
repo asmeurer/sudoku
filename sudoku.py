4b496e157035af90a6986981da9eaf46db6680dc
Aaron Meurer <asmeurer@gmail.com>
2015-09-17 23:49:30 -0500

    Encode true/false in the build string
    That way there is only one version and conda doesn't have to minimize the
    version equation.

--- sudoku.py
+++ sudoku.py
@@ -28,16 +28,16 @@ REPODATA = {
   "packages": {}
 }
 
-def generate_info(name, version, depends):
+def generate_info(name, build_string, depends):
     return {
-        "{name}-{version}-0.tar.bz2".format(name=name, version=version): {
-            "build": "0",
+        "{name}-a-{build_string}.tar.bz2".format(name=name, build_string=build_string): {
+            "build": build_string,
             "build_number": 0,
             "date": datetime.date.today().strftime("%Y-%m-%d"),
             "depends": depends,
             "name": name,
             "size": 0,
-            "version": str(version)
+            "version": 'a'
             }
     }
 
@@ -52,21 +52,21 @@ def generate_cells():
                         continue
                     # Each entry being set (version 1) requires that the other
                     # entries are not set (version 0)
-                    depends.append("%sx%s-is-%s 0" % (row, column, d))
+                    depends.append("%sx%s-is-%s a f" % (row, column, d))
 
                 for other_row in range(1, 10):
                     if other_row == row:
                         continue
                     # If an entry is set, other cells in the same column can't
                     # have the same entry.
-                    depends.append("%sx%s-is-%s 0" % (other_row, column, entry))
+                    depends.append("%sx%s-is-%s a f" % (other_row, column, entry))
 
                 for other_column in range(1, 10):
                     if other_column == column:
                         continue
                     # If an entry is set, other cells in the same row can't
                     # have the same entry.
-                    depends.append("%sx%s-is-%s 0" % (row, other_column, entry))
+                    depends.append("%sx%s-is-%s a f" % (row, other_column, entry))
 
                 # x - (x - 1)%3 is the largest of 1, 4, 7 that is less than x
                 top_corner = (row - (row - 1)%3, column - (column - 1)%3)
@@ -76,10 +76,10 @@ def generate_cells():
                         continue
                     # If an entry is set, other cells in the same 3x3 square
                     # can't have the same entry.
-                    depends.append("%sx%s-is-%s 0" % (cell[0], cell[1], entry))
+                    depends.append("%sx%s-is-%s a f" % (cell[0], cell[1], entry))
 
-                p1 = generate_info("%sx%s-is-%s" % (row, column, entry), 1, depends)
-                p0 = generate_info("%sx%s-is-%s" % (row, column, entry), 0, [])
+                p1 = generate_info("%sx%s-is-%s" % (row, column, entry), 't', depends)
+                p0 = generate_info("%sx%s-is-%s" % (row, column, entry), 'f', [])
                 packages.update({**p0, **p1})
 
     return packages
@@ -92,7 +92,7 @@ def generate_cell_metapackages():
     for row in range(1, 10):
         for column in range(1, 10):
             for entry in range(1, 10):
-                p = generate_info("cell-%sx%s" % (row, column), entry,
+                p = generate_info("cell-%sx%s" % (row, column), str(entry),
                     ["%sx%s-is-%s" % (row, column, entry)])
                 packages.update(p)
 
@@ -103,7 +103,7 @@ def generate_cell_metapackages():
 # metapackages.
 
 def generate_sudoku_metapackage():
-    return generate_info("sudoku", 0,
+    return generate_info("sudoku", '0',
         ["cell-%sx%s" % (row, column)
             for row in range(1, 10)
             for column in range(1, 10)])
