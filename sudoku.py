66ab823499c0c9036749692c47bd8d56a7033fe8
Aaron Meurer <asmeurer@gmail.com>
2015-09-17 22:14:50 -0500

    Add rule for 3x3 squares. Remove depends0.

--- sudoku.py
+++ sudoku.py
@@ -26,31 +26,38 @@ def generate_cells():
     for row in range(1, 10):
         for column in range(1, 10):
             for entry in range(1, 10):
-                depends1 = []
-                depends0 = []
+                depends = []
                 for d in range(1, 10):
                     if d == entry:
                         continue
                     # Each entry being set (version 1) requires that the other
                     # entries are not set (version 0)
-                    depends1.append("%sx%s-is-%s 0" % (row, column, d))
+                    depends.append("%sx%s-is-%s 0" % (row, column, d))
 
                 for other_row in range(1, 10):
                     if other_row == row:
                         continue
                     # If an entry is set, other cells in the same column can't
                     # have the same entry.
-                    depends1.append("%sx%s-is-%s 0" % (other_row, column, entry))
+                    depends.append("%sx%s-is-%s 0" % (other_row, column, entry))
 
                 for other_column in range(1, 10):
                     if other_column == column:
                         continue
                     # If an entry is set, other cells in the same row can't
                     # have the same entry.
-                    depends1.append("%sx%s-is-%s 0" % (row, other_column, entry))
+                    depends.append("%sx%s-is-%s 0" % (row, other_column, entry))
 
-                p1 = generate_info("%sx%s-is-%s" % (row, column, entry), 1, depends1)
-                p0 = generate_info("%sx%s-is-%s" % (row, column, entry), 0, depends0)
+                # x - (x - 1)%3 is the largest of 1, 4, 7 that is less than x
+                top_corner = (row - (row - 1)%3, column - (column - 1)%3)
+                for i in range(9):
+                    cell = (top_corner[0] + i//3, top_corner[1] + i%3)
+                    # If an entry is set, other cells in the same 3x3 square
+                    # can't have the same entry.
+                    depends.append("%sx%s-is-%s 0" % (cell[0], cell[1], entry))
+
+                p1 = generate_info("%sx%s-is-%s" % (row, column, entry), 1, depends)
+                p0 = generate_info("%sx%s-is-%s" % (row, column, entry), 0, [])
                 packages.update({**p0, **p1})
 
     return packages
