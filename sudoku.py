cbcb89a7a3ee4884768e272bbe3435bb6e08d224
Aaron Meurer <asmeurer@gmail.com>
2015-09-17 22:03:02 -0500

    Add constraints for the same column and the same row

--- sudoku.py
+++ sudoku.py
@@ -26,16 +26,31 @@ def generate_cells():
     for row in range(1, 10):
         for column in range(1, 10):
             for entry in range(1, 10):
-                depends = []
+                depends1 = []
+                depends0 = []
                 for d in range(1, 10):
                     if d == entry:
                         continue
-                    # Each entry being 1 requires that the other entries be 0
-                    depends.append("%sx%s-is-%s 0" % (row, column, d))
+                    # Each entry being set (version 1) requires that the other
+                    # entries are not set (version 0)
+                    depends1.append("%sx%s-is-%s 0" % (row, column, d))
 
-                p1 = generate_info("%sx%s-is-%s" % (row, column, entry), 1,
-                    depends)
-                p0 = generate_info("%sx%s-is-%s" % (row, column, entry), 0, [])
+                for other_row in range(1, 10):
+                    if other_row == row:
+                        continue
+                    # If an entry is set, other cells in the same column can't
+                    # have the same entry.
+                    depends1.append("%sx%s-is-%s 0" % (other_row, column, entry))
+
+                for other_column in range(1, 10):
+                    if other_column == column:
+                        continue
+                    # If an entry is set, other cells in the same row can't
+                    # have the same entry.
+                    depends1.append("%sx%s-is-%s 0" % (row, other_column, entry))
+
+                p1 = generate_info("%sx%s-is-%s" % (row, column, entry), 1, depends1)
+                p0 = generate_info("%sx%s-is-%s" % (row, column, entry), 0, depends0)
                 packages.update({**p0, **p1})
 
     return packages
